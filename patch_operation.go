20e2b0b26697bb0962e11ee9491cd5d0a5d44f4c
Gerrit Renker <gerrit@erg.abdn.ac.uk>
2015-11-05 19:03:29 -0700

    Separate patch operation

---
+++ patch_operation.go
@@ -0,0 +1,22 @@
+package clcv2
+
+type PatchOperation struct {
+	// The operation to perform on a given property of the resource.
+	Op	string		`json:"op"`
+
+	// The property of the resource to perform the operation on.
+	Member	string		`json:"member"`
+
+	// The value to patch - depends on the type of operation.
+	Value	interface{}	`json:"value"`
+}
+
+// Run patch operation(s) @ops and return Link status.
+func (c *Client) patchStatus(path string, ops ...*PatchOperation) (statusId string, err error) {
+	return c.getStatus("PATCH", path, ops)
+}
+
+// Like patchStatus(), but without statusId. For those patch operations that return '204 No Content'.
+func (c *Client) patch(path string, ops ...*PatchOperation) error {
+	return c.getResponse("PATCH", path, ops, nil)
+}
