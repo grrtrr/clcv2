971f23fc7773f8f9855c71574466a5a2380268e2
Gerrit Renker <Gerrit.Renker@ctl.io>
2015-11-12 11:47:04 -0700

    groups: allow to delete by name (when unambiguous), too

--- examples/group/delete.go
+++ examples/group/delete.go
@@ -6,6 +6,7 @@ package main
 import (
 	"github.com/grrtrr/clcv2"
 	"github.com/grrtrr/exit"
+	"encoding/hex"
 	"path"
 	"flag"
 	"fmt"
@@ -13,8 +14,11 @@ import (
 )
 
 func main() {
+	var uuid string	/* UUID of the HW group to delete */
+	var location = flag.String("l", "", "Data center location if using HW Group-Name")
+
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "usage: %s [options]  <HW Group UUID>\n", path.Base(os.Args[0]))
+		fmt.Fprintf(os.Stderr, "usage: %s [options]  <HW Group-Name or UUID>\n", path.Base(os.Args[0]))
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -28,10 +32,26 @@ func main() {
 		exit.Fatal(err.Error())
 	}
 
-	reqId, err := client.DeleteGroup(flag.Arg(0))
+	/* If the first argument decodes as a hex value, assume it is a Hardware Group UUID */
+	if _, err := hex.DecodeString(flag.Arg(0)); err == nil {
+		uuid = flag.Arg(0)
+	} else if *location == "" {
+		exit.Errorf("Need a location (-l argument) when not using a HW Group UUID")
+	} else {
+		fmt.Printf("Resolving group UUID of %s in %s ...\n", flag.Arg(0), *location)
+		if grp, err := client.GetGroupByName(flag.Arg(0), *location); err != nil {
+			exit.Errorf("Failed to resolve group name %q: %s", flag.Arg(0), err)
+		} else if grp == nil {
+			exit.Errorf("No group named %q was found on %s", flag.Arg(0), *location)
+		} else {
+			uuid = grp.Id
+		}
+	}
+
+	reqId, err := client.DeleteGroup(uuid)
 	if err != nil {
 		exit.Fatalf("Failed to delete hardware group: %s", err)
 	}
 
-	fmt.Printf("Status ID for %s deletion request: %s\n", flag.Arg(0), reqId)
+	fmt.Printf("Status ID for group deletion: %s\n", reqId)
 }
