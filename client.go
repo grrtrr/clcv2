5332144927d099d9c67188a01fd6f0860ad3cd91
Gerrit Renker <Gerrit.Renker@ctl.io>
2016-04-06 10:04:57 -0600

    use longer flag names for client initialization, for clarity

--- client.go
+++ client.go
@@ -40,9 +40,9 @@ var (
 
 func init() {
 	flag.BoolVar(&g_debug, "d", false, "Produce debug output")
-	flag.StringVar(&g_user, "u", "", "CLC Login Username")
-	flag.StringVar(&g_pass, "p", "", "CLC Login Password")
-	flag.StringVar(&g_acct, "a", "", "CLC Account Alias to use (instead of default)")
+	flag.StringVar(&g_user, "username", "", "CLC Login Username")
+	flag.StringVar(&g_pass, "password", "", "CLC Login Password")
+	flag.StringVar(&g_acct, "account",  "", "CLC Account Alias to use (instead of default)")
 	/*
 	 * Caveat: keep the timeout value high, at least a few minutes.
 	 *         Some operations, such as querying details of a new server immediately
