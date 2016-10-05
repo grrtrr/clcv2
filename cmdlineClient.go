grrtrr/clcv2

commit 7cc87e3532694b47011cb0046b21e7529d4e70f7

refactor/consolidate the use of environment variables

cmdlineClient.go
@@ -47,8 +47,9 @@ type CLIClient struct {
 
 // NewCLIClient returns an authenticated commandline client.
 // This will use the default values for AccountAlias  and LocationAlias.
-// It will respect the following environment variable to override the defaults:
-// - CLC_ACCOUNT: takes precedence over default AccountAlias
+// It will respect the following environment variables to override the defaults:
+// - CLC_ACCOUNT:  takes precedence over default AccountAlias
+// - CLC_BASE_URL: overrides the API URL (for testing)
 func NewCLIClient() (client *CLIClient, err error) {
 	username, password, err := resolveUserAndPass()
 	if err != nil {
@@ -57,46 +58,32 @@ func NewCLIClient() (client *CLIClient, err error) {
 	client = &CLIClient{initClient(username, password)}
 
 	client.credentialsChanged = client.saveCredentials
-
 	if g_debug {
 		client.Log = log.New(os.Stdout, "", log.Ltime|log.Lshortfile)
 	}
 
-	if err = setBaseURL(); err != nil {
-		return nil, err
-	}
-
-	if err = client.loadCredentials(); err != nil {
-		return nil, err
-	}
-
-	/* Set/override account alias. */
-	client.AccountAlias = client.credentials.AccountAlias
-
-	if account := os.Getenv("CLC_ACCOUNT"); account != "" {
-		client.AccountAlias = account
-	}
-
-	/* Commandline flags take precedence over environment variables. */
-	if g_acct != "" {
-		client.AccountAlias = g_acct
-	}
-	return client, nil
-}
-
-// setBaseURL sets the URL base based on $CLC_BASE_URL.
-func setBaseURL() error {
+	// Set/override %baseURL
 	if envURL := os.Getenv("CLC_BASE_URL"); envURL != "" {
 		url, err := url.Parse(envURL)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		if url.Scheme == "" {
 			url.Scheme = "https"
 		}
 		baseURL = url.String()
 	}
-	return nil
+
+	// Set/override account alias.
+	if g_acct != "" {
+		client.AccountAlias = g_acct
+	} else if account := os.Getenv("CLC_ACCOUNT"); account != "" {
+		client.AccountAlias = account
+	} else { // may have been initialized from disk
+		client.AccountAlias = client.credentials.AccountAlias
+	}
+
+	return client, nil
 }
 
 // Populate and allocate c.credentials, either by loading from file or via a fresh login.

client.go
@@ -132,12 +132,14 @@ func initClient(user, pass string) *Client {
 	return client
 }
 
-// Log in and update credentials if successful.
+// login wipes olds credentials, logs in, and updates credentials if successful.
 func (c *Client) login() error {
+	c.credentials = new(LoginRes)
+
 	if c.LoginReq.Username == "" || c.LoginReq.Password == "" {
 		return fmt.Errorf("invalid CLC credentials %q/%q", c.LoginReq.Username, c.LoginReq.Password)
 	}
-	c.credentials = new(LoginRes)
+
 	if err := c.getCLCResponse("POST", "/v2/authentication/login", &c.LoginReq, c.credentials); err != nil {
 		return err
 	}

commit 58f5e273c0c7a2bc5e68d530bb09418e973cddab

fix missing loadCredentials (was broken by 7cc87e3532694b47011cb0046b21e7529d4e70f7)

cmdlineClient.go
@@ -74,6 +74,10 @@ func NewCLIClient() (client *CLIClient, err error) {
 		baseURL = url.String()
 	}
 
+	if err = client.loadCredentials(); err != nil {
+		return nil, err
+	}
+
 	// Set/override account alias.
 	if g_acct != "" {
 		client.AccountAlias = g_acct