e449353fe71cf9524c3eac81c4653764ac78ecae
Gerrit Renker <Gerrit.Renker@ctl.io>
2017-01-05 12:43:43 -0700

    list available networks

---
+++ examples/clconsole/cmd/networks.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/grrtrr/clcv2"
+	"github.com/grrtrr/exit"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	Root.AddCommand(&cobra.Command{
+		Use:     "networks [location]",
+		Aliases: []string{"net"},
+		Short:   "Show available networks",
+		Long:    "Show networks available to current account. If @location argument is present, it overrides the default data centre location.",
+		Run: func(cmd *cobra.Command, args []string) {
+			var dataCentre = location
+
+			if len(args) > 0 {
+				dataCentre = args[0]
+			}
+
+			fmt.Printf("Networks visible to %s account in %s:\n", client.AccountAlias, strings.ToUpper(dataCentre))
+			showNetworks(client, dataCentre, client.AccountAlias)
+			if client.AccountAlias != client.RegisteredAccountAlias() {
+				fmt.Printf("Networks visible to parent %s account:\n", client.RegisteredAccountAlias())
+				showNetworks(client, dataCentre, client.RegisteredAccountAlias())
+			}
+		},
+	})
+}
+
+// showNetworks shows networks visible to @account in data centre location @location.
+func showNetworks(client *clcv2.CLIClient, location, account string) {
+	networks, err := client.GetNetworks(location, account)
+	if err != nil {
+		exit.Fatalf("failed to list networks in %s: %s", location, err)
+	}
+
+	if len(networks) == 0 {
+		println("Empty result.")
+	} else {
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetAutoFormatHeaders(false)
+		table.SetAlignment(tablewriter.ALIGN_RIGHT)
+		table.SetAutoWrapText(false)
+
+		table.SetHeader([]string{"CIDR", "Gateway", "VLAN", "Name", "Description", "Type", "ID"})
+		for _, l := range networks {
+			table.Append([]string{l.Cidr, l.Gateway, fmt.Sprint(l.Vlan), l.Name, l.Description, l.Type, l.Id})
+		}
+		table.Render()
+	}
+}
