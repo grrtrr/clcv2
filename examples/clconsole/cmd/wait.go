5b79fe14f62ddf1bc714a65762433185268c8c70
Gerrit Renker <Gerrit.Renker@ctl.io>
2017-01-05 14:33:27 -0700

    await queue status

---
+++ examples/clconsole/cmd/wait.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/grrtrr/clcv2"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	Root.AddCommand(&cobra.Command{
+		Use:     "wait  <statusID>",
+		Short:   "Await completion of queue job and report status",
+		PreRunE: checkArgs(1, "Need a status ID to poll"),
+		Run: func(cmd *cobra.Command, args []string) {
+			client.PollStatusFn(args[0], intvl, func(s clcv2.QueueStatus) {
+				log.Printf("%s: %s", args[0], s)
+			})
+		},
+	})
+}
