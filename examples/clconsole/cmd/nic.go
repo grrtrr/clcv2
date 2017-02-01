ad2f0fe1005ad04966319e39bb5e92faa43841fe
Gerrit Renker <Gerrit.Renker@ctl.io>
2017-02-01 10:31:39 -0700

    improve help message

--- examples/clconsole/cmd/nic.go
+++ examples/clconsole/cmd/nic.go
@@ -17,7 +17,8 @@ func init() {
 			PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
 				// Both commands take <serverName> <net ID | name | CIDR>
 				if len(args) < 2 {
-					return errors.Errorf("Need a server name and a network ID or CIDR  for the secondary NIC")
+					return errors.Errorf("Need a server name and a network specifier " +
+						"(network ID/name/CIDR, or IP on the network) for the secondary NIC")
 				}
 				setLocationBasedOnServerName(args[0])
 				return nil
@@ -29,9 +30,9 @@ func init() {
 		}
 
 		addNIC = &cobra.Command{
-			Use:   "add  <serverName>  <net (ID | CIDR | IP)>",
+			Use:   "add  <serverName>  <net (ID | Name | CIDR | IP)>",
 			Short: "Add a secondary NIC to server",
-			Long:  "Add a secondary NIC to @server on network @net (using network ID, CIDR, or IP)",
+			Long:  "Add a secondary NIC to @server on network @net (using network ID, name, CIDR, or and IP on the network)",
 			Run: func(cmd *cobra.Command, args []string) {
 				var server, netID = args[0], args[1]
 
@@ -46,14 +47,15 @@ func init() {
 				if err = client.ServerAddNic(server, netID, addNICFlags.ip); err != nil {
 					log.Fatalf("failed to add NIC to %s: %s", server, err)
 				}
+				log.Printf("Successfully added NIC to server %s", server)
 			},
 		}
 
 		removeNIC = &cobra.Command{
-			Use:     "remove  <serverName>  <net (ID | CIDR | IP)>",
+			Use:     "remove  <serverName>  <net (ID | Name | CIDR | IP)>",
 			Aliases: []string{"rm", "del"},
 			Short:   "Remove secondary NIC from server",
-			Long:    "Remove secondary NIC identified by @net (network ID, CIDR, or IP) from @serverName",
+			Long:    "Remove secondary NIC identified by @net (network ID, name, CIDR, or an IP on the network) from @serverName",
 			Run: func(cmd *cobra.Command, args []string) {
 				var server, netID = args[0], args[1]
 
@@ -68,6 +70,7 @@ func init() {
 				if err = client.ServerDelNic(server, netID); err != nil {
 					log.Fatalf("failed to remove NIC from %s: %s", server, err)
 				}
+				log.Printf("Successfully removed NIC from server %s", server)
 			},
 		}
 	)
