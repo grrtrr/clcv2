42be61b05494443eac34e040a9b92e4658a3f0bf
Gerrit Renker <Gerrit.Renker@ctl.io>
2017-03-18 10:09:23 -0600

    improve snapshot creation - log if an existing snapshot has to be deleted first

--- examples/clconsole/cmd/snapshot.go
+++ examples/clconsole/cmd/snapshot.go
@@ -4,19 +4,38 @@ package cmd
  * Snapshot operations
  */
 import (
+	"log"
+
+	"github.com/grrtrr/clcv2"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
+// snapCreateFlags contains flags pertaining to creating a new snapshot
+var snapCreateFlags struct {
+	days int // Number of days to keep the snapshot for
+}
+
 func init() {
-	Root.AddCommand(&cobra.Command{
+	// Create snapshot
+	var createSnapshot = &cobra.Command{
 		Use:     "snapshot  [group|server [group|server]...]",
 		Aliases: []string{"snap", "sn"},
 		Short:   "Snapshot server(s)",
-		Long:    "Create new server snapshot, replacing older snapshots if they exist",
+		Long:    "Create new server snapshot, replacing previous snapshot if it exists",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			if snapCreateFlags.days <= 0 || snapCreateFlags.days > 10 {
+				return errors.Errorf("Invalid number of days %d - must be in the range 1..10", snapCreateFlags.days)
+			}
+			return nil
+		},
 		Run: func(cmd *cobra.Command, args []string) {
-			serverCmd("snapshot", client.SnapshotServer, args)
-		}})
+			serverCmd("create new snapshot", snapshotHandler, args)
+		}}
+	createSnapshot.Flags().IntVar(&snapCreateFlags.days, "days", 10, "Number of days to keep the snapshot for")
+	Root.AddCommand(createSnapshot)
 
+	// Delete snapshot
 	Root.AddCommand(&cobra.Command{
 		Use:     "delsnap  [group|server [group|server]...]",
 		Aliases: []string{"rmsnap", "delete-snapshot"},
@@ -26,6 +45,7 @@ func init() {
 			serverCmd("delete snapshot", client.DeleteSnapshot, args)
 		}})
 
+	// Revert to snapshot
 	Root.AddCommand(&cobra.Command{
 		Use:     "revert  [group|server [group|server]...]",
 		Aliases: []string{"rev"},
@@ -34,4 +54,19 @@ func init() {
 		Run: func(cmd *cobra.Command, args []string) {
 			serverCmd("revert to snapshot", client.RevertToSnapshot, args)
 		}})
+}
+
+// snapshotHandler is a convenience function to automatically delete an existing snapshot before creating a new one.
+// This is to satisfy the use case "I want to snapshot what I just did", without having to run multiple commands.
+// Note: relies on 'client' and 'intvl' global variables.
+func snapshotHandler(serverId string) (reqId string, err error) {
+	if reqID, err := client.DeleteSnapshot(serverId); err == nil {
+		log.Printf("%s delete current snapshot: %s", serverId, reqID)
+		client.PollStatusFn(reqID, intvl, func(s clcv2.QueueStatus) {
+			log.Printf("%s delete current snapshot: %s", serverId, s)
+		})
+	} else if err != clcv2.ErrNoSnapshot {
+		return "", errors.Errorf("%s: failed to delete existing snapshot: %s", serverId, err)
+	}
+	return client.CreateSnapshot(serverId, snapCreateFlags.days)
 }
